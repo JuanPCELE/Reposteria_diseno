2a1f1728006e9d7b3c4c23013f2346eb9d0cf89b
JuanPablo <juan.castaneda.jp@gmail.com>
2026-04-20 21:08:23 -0600

    Error Fix

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,12 +1,12 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
- 
+
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        pasteles: resolve(__dirname, 'Pasteles.html'), 
+        pasteles: resolve(__dirname, 'Secciones/Pasteles.html'), 
       },
     },
   },
